bcd62c6857c26356dcb91f1469bf3243600735f9
Elliot Pettingale <elliot@smartercoding.co.uk>
2023-12-03 06:14:16 +0000

    add form submit

--- components/blocks/GravityForm/index.tsx
+++ components/blocks/GravityForm/index.tsx
@@ -1,13 +1,32 @@
 import Button from "@/components/common/Button";
 import Input from "@/components/common/Input";
+import { useState } from "react";
 
 interface Props {}
 
 export default function GravityForm({}: Props) {
+  const [submitted, setSubmitted] = useState(false);
+
+  const onSubmit = (event: any) => {
+    event.preventDefault();
+
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 300);
+  };
+
+  if (submitted) {
+    return (
+      <div>
+        <p>Thank you, we'll be in touch.</p>
+      </div>
+    );
+  }
+
   return (
-    <form className="w-full">
-      <Input type="text" placeholder="Your Name" />
-      <Input type="text" placeholder="Your Email Address" />
+    <form className="w-full" onSubmit={onSubmit}>
+      <Input type="text" placeholder="Your Name" required />
+      <Input type="text" placeholder="Your Email Address" required />
       <div className="text-right">
         <Button>Submit</Button>
       </div>
